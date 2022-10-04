e37b5a116e05d6b3791e5bfa9b6e5d81ebdc13db
gyuchanlee <hoo788@gmail.com>
2022-10-04 19:41:18 +0900

    2022-10-04 첫 프로젝트 구성

---
+++ src/main/java/com/dodo/dodobirdWorld/DodobirdWorldApplication.java
@@ -0,0 +1,13 @@
+package com.dodo.dodobirdWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodobirdWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodobirdWorldApplication.class, args);
+	}
+
+}
