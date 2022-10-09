23ae016a6fae15c422cf68c6a9d80a09dc386605
gyuchanlee <hoo788@gmail.com>
2022-10-09 15:11:02 +0900

    2022-10-09 @transactional

--- src/main/java/com/dodo/dodobirdWorld/board/controller/BoardController.java
+++ src/main/java/com/dodo/dodobirdWorld/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class BoardController {
 	}
 	
 	// 글 상세 읽기 페이지 이동
+	@Transactional(rollbackFor = Exception.class)
 	@GetMapping(value="/board/{board_id}")
 	public ModelAndView boardOne(@PathVariable(value = "board_id", required = true) String board_id) {
 		ModelAndView view = new ModelAndView("boardOne");
